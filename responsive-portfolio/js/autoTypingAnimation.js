52cea6af6d7f671eff2eb5bc3ca0881db59d2058
LIZA <114486225+liza0012@users.noreply.github.com>
2024-06-20 02:48:54 +0530

    Add files via upload

---
+++ responsive-portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Liza</span> .I enjoy problem solving and integrating current technologies to solve real-world problems. "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
